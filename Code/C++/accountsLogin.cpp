70a5b2ea6145300100a3d176cd16509da47125a6
louiepuppy <154765282+louiepuppy@users.noreply.github.com>
2024-02-22 15:50:14 -0600

    updating

--- Code/C++/accountsLogin.cpp
+++ Code/C++/accountsLogin.cpp
@@ -2,15 +2,55 @@
 #include <string>
 #include <vector>
 
+// Login function
+static bool login_handler(std::string user, std::string pass, bool logged) {
+	if (user == "admin" && pass == "<PASSWORD>") {
+		return logged == true;
+	}
+	else {
+		return logged == false;
+	}
+}
+
+
 // registration/login program
 int main() {
 
 	std::string username;
 	std::string password;
 	short choice;
+	bool loggedIn = false;
 
-	// menu
-	std::cout << "********MENU********\n" << "";
+	while (true) {
+		if (!loggedIn) {
+			// menu
+			std::cout << "********MENU********\n" << "1. Login\n" << "2. Register\n" << "5. Exit\n";
+			std::cout << "********************\n" << "Enter your choice: ";
+			std::cin >> choice;
+			std::cout << std::endl;
+			// login
+			switch (choice) {
+			case 1:
+				std::cout << "Enter username: ";
+				std::cin >> username;
+				std::cout << "\n Hello, " << username << ".\nEnter password: ";
+				std::cin >> password;
+				std::cout << login_handler(username, password, loggedIn);
+				break;
+			default:
+				std::cout << "Invalid choice.";
+				break;
+			}
+		}
+		else {
+			// account menu
+			std::cout << "********ACCOUNT MENU********\n" << "1. Logout\n" << "2. Change Password\n" << "5. Exit\n";
+			std::cout << "********************\n" << "Enter your choice: ";
+			std::cin >> choice;
+			std::cout << std::endl;
+			// logout
+		}
+	}
 
 	return 0;
 }
