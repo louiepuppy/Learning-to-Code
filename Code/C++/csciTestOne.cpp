d3a0eae96e56839ed6c33c353b47826f19158078
louie <154765282+louiepuppy@users.noreply.github.com>
2024-03-04 13:41:46 -0600

    Update csciTestOne.cpp

--- Code/C++/csciTestOne.cpp
+++ Code/C++/csciTestOne.cpp
@@ -19,7 +19,7 @@ int main() {
 		return 1;
 	}
 
-	cout << "Here is the detailed information about the yard and the trees" << endl << endl;
+	cout << "Here is the detailed information about the yard and the trees:" << endl << endl;
 
 	while (!inFile.eof()) {
 		inFile >> yardLocation;
@@ -28,12 +28,12 @@ int main() {
 		inFile >> treeRadius;
 		inFile >> treeSpace;
 		cout << fixed << setprecision(2) << showpoint;
-		cout << setw(20) << "Yard Type: " << yardLocation << " " << yard << endl;
-		cout << setw(20) << "Yards Length: " << yardLength << endl;
-		cout << setw(20) << "Tree Radius: " << treeRadius << endl;
-		cout << setw(20) << "Distance between Trees: " << treeSpace << endl;
+		cout << setw(5) << " " << setw(25) << left << "Yard Type: " << setw(10) << internal << yardLocation << " " << yard << endl;
+		cout << setw(5) << " " << setw(25) << left << "Yards Length: " << setw(10) << internal << yardLength << endl;
+		cout << setw(5) << " " << setw(25) << left << "Tree Radius: " << setw(10) << internal << treeRadius << endl;
+		cout << setw(5) << " " << setw(25) << left << "Distance between Trees: " << setw(10) << internal << treeSpace << endl << endl;
 
-		treesToPlant = 120 / (treeRadius * 2 + treeSpace);
+		treesToPlant = yardLength / (treeRadius * 2 + treeSpace);
 		areaOccupiedByTrees = (3.14159 * pow(treeRadius, 2)) * treesToPlant;
 
 		cout << "The number of Trees that can be planted for " << yardLocation << " " << yard << " is " << treesToPlant << endl;
