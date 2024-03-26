0123748c42d6d44ff4a297cac3883cfd7a1729a7
louie <154765282+louiepuppy@users.noreply.github.com>
2024-03-26 09:54:27 -0500

    update

---
+++ Code/C++/arraysExample.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    const int NUM_EMPLOYEES = 6;
+    int hours[NUM_EMPLOYEES];
+    int count = 0;
+
+    ifstream inFile("/Data/arraysData.txt");
+
+    inFile.open("/Data/arrays.txt");
+    if (!inFile.is_open()) {
+        cout << "Could not open file";
+        return 1;
+    } else {
+        while (count < NUM_EMPLOYEES && inFile >> hours[count]) {
+            count++;
+        }
+        inFile.close();
+
+        cout << "The hours worked by each employee are\n";
+        for (int employee = 0; employee < count; employee++) {
+            cout << "Employee " << employee + 1 << ":";
+            cout << hours[employee] << endl;
+        }
+    }
+
+    return 0;
+}
